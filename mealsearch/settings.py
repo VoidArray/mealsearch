bb066f95d792c170e69d4042f3bd9f9795f454a9
redfury <asoloninin@splinex.com>
2016-05-05 13:04:16 +0500

    add models

--- mealsearch/settings.py
+++ mealsearch/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'search_page.apps.SearchPageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- search_page/models.py
+++ search_page/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=300)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class AlterIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, related_name='id_ingredient')
+    alternative = models.ForeignKey(Ingredient, related_name='id_complimentary_ingredient')
+
+class ListIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='id_recipe')
+    alteringredient = models.ForeignKey(AlterIngredient, related_name='id_ingridient_w_alter')
+
