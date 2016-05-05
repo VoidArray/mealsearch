6dc8f7fde0d479664090b02dfe19612ea36c0c92
redfury <asoloninin@splinex.com>
2016-05-05 16:42:04 +0500

    fix models, add urls

--- search_page/views.py
+++ search_page/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Ingredient, Recipe, AlterIngredient, ListIngredient
 
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def search_result(request, search):
+    ingerdient_list = Ingredient.objects.all()
+    output = ', '.join([i.name for i in ingerdient_list])
+    return HttpResponse(output + '\n' + search)
