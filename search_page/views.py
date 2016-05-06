398bb31ba8de387bc5d8da93326ed9898b2d19b9
redfury <asoloninin@splinex.com>
2016-05-06 16:55:59 +0500

    add angularjs

--- search_page/views.py
+++ search_page/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -8,6 +9,24 @@ def index(request):
     return HttpResponse("Hello, world!")
 
 def search_result(request, search):
-    ingerdient_list = Ingredient.objects.all()
-    output = ', '.join([i.name for i in ingerdient_list])
-    return HttpResponse(output + '\n' + search)
+    # result_dict['meals'] = [{'name':'1', 'ingredients':'2'}, {'name':'3', 'ingredients':'4'}, {'name':'3', 'ingredients':'4'}]
+    result_dict = {}
+    meals_list = []
+    result_dict['count'] = len(Recipe.objects.all())
+    list_recipes_w_ing = ListIngredient.objects.all()
+    for recipe in list_recipes_w_ing:
+        meal = {}
+        meal['name'] = recipe.recipe.name
+        meal['ingredients'] = [i.name for i in recipe.ingredient.all()]
+        for alt_ingr in  recipe.alteringredient.all():
+            alternative_list = [a.name for a in alt_ingr.alternative.all()]
+            alt_ingredients = '{} или {}'.format(alt_ingr.ingredient.name, ' | '.join(alternative_list))
+            meal['ingredients'].append(alt_ingredients)
+        meals_list.append(meal)
+    result_dict['meals'] = meals_list
+    if search:
+        print(result_dict)
+        return HttpResponse(json.dumps(result_dict, ensure_ascii=False),
+                            content_type="application/json; encoding=utf-8")
+    else:
+        return render(request, 'index.html')
